ba54021e7f2a1e6e68f82c4bc2cb3982e59c602d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-20 00:40:49 +0200

    Fix None check in sum_list function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,4 +7,8 @@ import sys
 
 def sum_list(input_list: list[float]) -> float:
     """sum_list function"""
-    return sum(input_list)
+    if input_list is None:
+      return 0
+    else:
+      return sum(input_list)
+
