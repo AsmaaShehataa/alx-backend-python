0513a073dec9c6a3b2314c59cd0a0c6574104e1c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-20 01:03:11 +0200

    Fix type annotation in element_length function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,6 +5,6 @@
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return a list of tuples"""
     return [(i, len(i)) for i in lst]
