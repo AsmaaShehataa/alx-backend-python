b3ac7f39a79eec0a36acade248569e8e5313d573
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-20 01:01:24 +0200

    Update to_kv function to return the original key and the square of the value

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """first element as the int and the second as the float"""
-    return (int(k), k ** 2)
+    return k, v ** 2
