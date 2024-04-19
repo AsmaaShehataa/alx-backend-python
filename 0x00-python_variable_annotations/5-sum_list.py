80bda761d367ff7728992f736d454f96d54585d1
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-20 00:46:26 +0200

    Fix type annotation in sum_list function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-"""type-annotated function sum_list"""
+"""Type-annotated function sum_list"""
+
+
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """sum_list function"""
+    """Sum_list function"""
     if input_list is None:
-      return 0
+        return 0
     else:
-      return sum(input_list)
-
+        return sum(input_list)
