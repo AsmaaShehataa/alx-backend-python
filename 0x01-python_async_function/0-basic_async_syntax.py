e37cfea0a4e4b86b07d9fdcc139d7331ff757a0f
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-22 14:17:15 +0200

    Add basic_async_syntax.py and main.py to 0x01-python_async_function directory

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that takes in an integer argument"""
+
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10):
+    """asynchronous coroutine that takes in an integer argument"""
+    random_delay = random.uniform(0, max_delay)
+    #wait_random = random.randrange(10, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
+
+
