0d98b5eb54b7c1b309a096d6778db845f7ac0a97
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-23 16:01:08 +0200

    Add type hint to async_generator function in 0x02-python_async_comprehension/0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[any, any, any]:
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
