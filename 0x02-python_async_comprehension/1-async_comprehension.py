4a325b85c501a8d9cecc0a35b3cfb7c0de9aaedf
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-23 16:18:42 +0200

    Refactor async_comprehension function to use a list comprehension instead of a generator expression in 0x02-python_async_comprehension/1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,12 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import List
 
 
 sync_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Async Generator"""
-    rand_nums = [i async for i in sync_generator()]
-    return rand_nums
-
+    return [i async for i in sync_generator()]
