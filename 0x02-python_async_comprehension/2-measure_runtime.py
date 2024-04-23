8099812eef053a4a1cb670b9c887f35b2c3b6220
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-23 16:56:46 +0200

    Add async runtime measurement in 0x02-python_async_comprehension/2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Python Async Comprehension"""
+
+import asyncio
+import random
+from typing import List
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """Measure runtime"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end = time.time() - start
+    return end
