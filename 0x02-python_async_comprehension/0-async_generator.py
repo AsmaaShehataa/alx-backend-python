f372484d35d2b9bb48b8546196107213ca7b8f9d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-23 15:48:10 +0200

    Add async generator and main script for Python async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Python Async Comprehntion"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async Generator"""
+    for _ in range(10):
+      await asyncio.sleep(1)
+      yield random.uniform(0, 10)
