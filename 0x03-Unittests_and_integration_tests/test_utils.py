b7b28081e4249f568ad65a0f667cbac15bbbb768
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-11 18:26:50 +0300

    Refactor access_nested_map function to use tuple instead of list for path parameter

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+    @parameterized.expand([
+      ({}, ("a",)),
+      ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+      """Test exception raised by access_nested"""
+      with self.assertRaises(KeyError):
+        access_nested_map(nested_map, path)
